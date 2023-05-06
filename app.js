4c7ee84a73a4f03bfe189b1a49ed036930dd88b6
wahidfebr <wahidfebr@gmail.com>
2023-05-06 17:58:15 +0700

    feat: add middlewares router and errorHandler

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-// const router = require("./routes");
-// const { errorHandler } = require("./middlewares");
 const app = express();
+const router = require("./routes");
+const { errorHandler } = require("./middlewares");
 
 app.use(cors());
 app.use(morgan('combined'));
@@ -15,11 +15,7 @@ app.use(morgan('combined'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
-// app.use(router);
-
-// app.use(errorHandler);
+app.use(router);
+app.use(errorHandler);
 
 module.exports = app;
