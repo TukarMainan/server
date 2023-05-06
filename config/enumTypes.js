bb6857e3e81cb9f3d64df0570e72d6d86771a85c
wahidfebr <wahidfebr@gmail.com>
2023-05-06 09:59:53 +0700

    feat: add enum types config

---
+++ config/enumTypes.js
@@ -0,0 +1,10 @@
+const statusUserEnum = ["unverified", "verified", "suspend", "premium"];
+
+const conditionItemPostEnum = ["brand new", "like new", "lightly used", "well used", "heavily used"];
+const statusItemPostEnum = ["active", "complete", "suspend"];
+
+module.exports = {
+    statusUserEnum,
+    conditionItemPostEnum,
+    statusItemPostEnum,
+}
